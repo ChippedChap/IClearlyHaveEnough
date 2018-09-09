336addf2a89a4d31b51f4daa0120bb398d8ca0c3
ChippedChap <mikiel.theg@gmail.com>
2018-09-09 18:07:36 -0400

    Initial commit

---
+++ IClearlyHaveEnough/IClearlyHaveEnoughInit.cs
@@ -0,0 +1,16 @@
+﻿using Verse;
+using Harmony;
+using System.Reflection;
+
+namespace IClearlyHaveEnough
+{
+	[StaticConstructorOnStartup]
+	static class IClearlyHaveEnoughInit
+	{
+		static IClearlyHaveEnoughInit()
+		{
+			var harmonyInstance = HarmonyInstance.Create("com.chippedchap.iclearlyhaveenough");
+			harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+		}
+	}
+}
---
+++ IClearlyHaveEnough/MouseAttachmentPatcher.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using Harmony;
+using RimWorld;
+using Verse;
+
+namespace IClearlyHaveEnough
+{
+	[HarmonyPatch(typeof(Designator_Build))]
+	[HarmonyPatch("DrawMouseAttachments")]
+	class MouseAttachmentPatcher
+	{
+		static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			var instrList = new List<CodeInstruction>(instructions);
+			for (int i = 0; i < instrList.Count; i++)
+			{
+				if (instrList[i].opcode == OpCodes.Ldfld && instrList[i].operand == typeof(Map).GetField("resourceCounter")) continue;
+				if (instrList[i].opcode == OpCodes.Callvirt && instrList[i].operand == typeof(ResourceCounter).GetMethod("GetCount"))
+				{
+					yield return new CodeInstruction(OpCodes.Call, typeof(MouseAttachmentPatcher).GetMethod("GetPresentOnMap"));
+					continue;
+				}
+				if (instrList[i].opcode == OpCodes.Ldstr && instrList[i].operand.Equals("NotEnoughStoredLower"))
+				{
+					yield return new CodeInstruction(OpCodes.Ldstr, "NotEnoughPresentLower");
+					continue;
+				}
+				yield return instrList[i];
+			}
+		}
+
+		public static int GetPresentOnMap(Map map, ThingDef def)
+		{
+			if (def.resourceReadoutPriority == ResourceCountPriority.Uncounted) return 0;
+			List<Thing> thingsOfDef = map.listerThings.ThingsOfDef(def);
+			int amountPresent = 0;
+			for (int i = 0; i < thingsOfDef.Count; i++)
+			{
+				amountPresent += thingsOfDef[i].stackCount;
+			}
+			return amountPresent;
+		}
+	}
+}
