017dceb7ed24e36755a6d1504b7e5164577c75d2
ChippedChap <mikiel.theg@gmail.com>
2018-11-03 20:40:26 -0400

    Update resource count when architect tab is opened
    Still updates every 204 ticks for possible Blueprint Totals Tooltip integration

---
+++ IClearlyHaveEnough/UpdateOnArchitectOpen.cs
@@ -0,0 +1,16 @@
+﻿using Harmony;
+using RimWorld;
+using Verse;
+
+namespace IClearlyHaveEnough
+{
+	[HarmonyPatch(typeof(MainTabWindow_Architect))]
+	[HarmonyPatch("PostOpen")]
+	class UpdateOnArchitectOpen
+	{
+		static void Postfix()
+		{
+			Find.CurrentMap.GetComponent<AllResourcesCounter_MapComponent>().UpdateResourceCounts();
+		}
+	}
+}
